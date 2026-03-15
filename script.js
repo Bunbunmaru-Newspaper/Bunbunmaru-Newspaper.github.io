be801434aef27350f9e06c80624dc0cea165ed2b
Bunbunmaru-Newspaper <ayasheinbaum@gmail.com>
2026-03-14 20:03:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function languagechange() {
         document.getElementById("descriptionpart2").innerHTML = "Este es el lugar donde se encuentran artículos exclusivos al sitio web";
         document.getElementById("articletitle").innerHTML = "Forma de escritura nuevamente inventada revoluciona Gensokyo"
         document.getElementById("link").innerHTML = "Leer el artículo"
+                document.getElementById("copyright").innerHTML = "Todos los personajes fueron tomados de Touhou Project por Team Shanghai Alice. ZUN, por favor no me demandes al vacío"
         return
     }else{
         document.getElementById("title").innerHTML = "BUNBUNMARU NEWSPAPER";
@@ -17,6 +18,7 @@ function languagechange() {
         document.getElementById("descriptionpart2").innerHTML = "This is the place where you will find website-exclusive articles";
         document.getElementById("articletitle").innerHTML = "Newly invented writing form takes Gensokyo by storm"
         document.getElementById("link").innerHTML = "Read the article"
+        document.getElementById("copyright").innerHTML = "All characters are taken from touhou project by Team Shanghai Alice. Please don't sue my ass into oblivion ZUN"
         return
     }
 }
