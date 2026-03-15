14aae8902d5173cb18dcf4ebd6041bfe557b45ac
Bunbunmaru-Newspaper <ayasheinbaum@gmail.com>
2026-03-14 19:57:58 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function languagechange() {
     if(language % 2 == 0){
         document.getElementById("title").innerHTML = "PERIÓDICO BUNBUNMARU";
         document.getElementById("description").innerHTML = "Bienvenidos al periódico Bunbunmaru OFICIAL 100% real no feik!";
-        document.getElementById("descriptionpart2").innerHTML = "Esto es una recopilación de todos los artículos de Bunbunmaru pasados";
+        document.getElementById("descriptionpart2").innerHTML = "Este es el lugar donde se encuentran artículos exclusivos al sitio web";
         document.getElementById("articletitle").innerHTML = "Forma de escritura nuevamente inventada revoluciona Gensokyo"
         document.getElementById("link").innerHTML = "Leer el artículo"
         return
     }else{
         document.getElementById("title").innerHTML = "BUNBUNMARU NEWSPAPER";
         document.getElementById("description").innerHTML = "Welcome to the OFFICIAL 100% REAL bunbunmaru newsletter!";
-        document.getElementById("descriptionpart2").innerHTML = "This is a compilation of past bunbunmaru articles";
+        document.getElementById("descriptionpart2").innerHTML = "This is the place where you will find website-exclusive articles";
         document.getElementById("articletitle").innerHTML = "Newly invented writing form takes Gensokyo by storm"
         document.getElementById("link").innerHTML = "Read the article"
         return
